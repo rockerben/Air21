ddc601919beb4fe293197a8790ef37c1a2f1b0cf
Ben Cortez <ben@Bens-MacBook-Pro.local>
2011-12-15 13:32:20 +0800

    commit

---
+++ Air21/DetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//
+//  Air21 Mobile
+//
+//  Created by Ben Cortez on 7/1/11.
+//  Copyright 2011 RedMedia. All rights reserved.
+//
+
+@interface DetailViewController : UIViewController
+{
+
+    IBOutlet UIWebView* webView;
+}
+
+@property (nonatomic, retain) IBOutlet UIWebView* webView;
+
+@end
